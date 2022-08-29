bd2a91d6fb1b65c155fdd4aa1661c6c877e20fcf
Dmitrii Sadyrev <79851806+sa1mon92@users.noreply.github.com>
2022-08-29 14:24:36 +0300

    Initial Commit

---
+++ TestShop/TestShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestShopApp.swift
+//  TestShop
+//
+//  Created by Дмитрий Садырев on 29.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
